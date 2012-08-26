bea765caf4980dc3c2cfa46c6ae5aeea4f349f91
Marina Samuel <marina.samuel25@gmail.com>
2012-08-26 17:37:07 -0400

    Adding PracticeUI.

--- src/piano/prototypes/ui/marina/PracticeUI.java
+++ src/piano/prototypes/ui/marina/PracticeUI.java
@@ -1,5 +1,21 @@
 package piano.prototypes.ui.marina;
 
-public class PracticeUI {
+import java.awt.Graphics;
+import java.io.IOException;
 
+import javax.swing.JFrame;
+
+public class PracticeUI extends SubView {
+
+	SubView keyboard;
+
+	public PracticeUI(JFrame parentFrame) throws IOException {
+		super();
+
+		keyboard = new Keyboard(parentFrame);
+	}
+
+	public void paintComponent(Graphics g) {
+		keyboard.paintComponent(g);
+	}
 }
