4164de3d98f45c7b469e362c44d6739b82d8b8cc
Marina Samuel <marina.samuel25@gmail.com>
2012-08-26 17:39:14 -0400

    Adding Box object for highlighting songs (hackish).

---
+++ src/piano/prototypes/ui/marina/Box.java
@@ -0,0 +1,28 @@
+package piano.prototypes.ui.marina;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import piano.prototypes.ui.buttons.marina.Button;
+
+public class Box extends Button {
+	int x, y, width, height;
+
+	public Box(int x, int y, int width, int height) {
+		super("", x, y, width, height);
+	}
+
+	@Override
+	public void hoverOver() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hoverOut() {
+		// TODO Auto-generated method stub
+
+	}
+}
