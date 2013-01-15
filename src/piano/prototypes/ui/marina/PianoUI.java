a0aa979f8b16eb83414852c8482dfcaf012c493b
Marina Samuel <marina.samuel25@gmail.com>
2013-01-15 15:09:17 -0500

    PianoUI no longer uses images for piano drawing.

--- src/piano/prototypes/ui/marina/PianoUI.java
+++ src/piano/prototypes/ui/marina/PianoUI.java
@@ -1,45 +1,27 @@
 package piano.prototypes.ui.marina;
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-import piano.prototypes.ui.buttons.marina.StartingButton;
-
-public class PianoUI extends Drawing {
+public class PianoUI extends Drawing implements KeyPressedCallback {
 	
   final static float dash1[] = {10.0f};
-	
-	BufferedImage bgImage, main_image, hover_play, hover_practice;
+
 	Drawing play, practice;
 	
-	ArrayList<StartingButton> buttons = new ArrayList<StartingButton>();
+	private KeyboardView keyboard;
 
 	public PianoUI(JFrame parentFrame) throws IOException {
 		super();
 
-		this.play = new PlayUI(parentFrame, this);
-		this.practice = new PracticeUI(parentFrame);
-		
-		// Import piano image.
-		File f  = new File("resources/piano2.jpg");
-		File f1 = new File("resources/hover_play.jpg");
-		File f2 = new File("resources/hover_practice.jpg");
-
-    bgImage        = ImageIO.read(f);
-    hover_play     = ImageIO.read(f1);
-    hover_practice = ImageIO.read(f2);
-    main_image     = bgImage;
-		
-		buttons.add(new StartingButton("Play", 600, 250, 150, 60, this, hover_play, play, parentFrame));
-		buttons.add(new StartingButton("Practice", 600, 470, 150, 60, this, hover_practice, practice, parentFrame));
+		HashMap<Integer, PianoMenuData> menuData = new HashMap<Integer, PianoMenuData>();
+		menuData.put(2, new PianoMenuData("Play", new PlayUI(parentFrame, this)));
+		menuData.put(4, new PianoMenuData("Ear Training", new EarTrainingUI(parentFrame, this)));
+		keyboard = new KeyboardView(this, true, this, menuData, parentFrame);
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -65,33 +47,23 @@ public class PianoUI extends Drawing {
 		g.drawString("By: Mastodon", 20, 300);
     
 		// Draw piano image
-    g.drawImage(bgImage, 250, 0, getWidth(), getHeight(), 0, 0, bgImage.getWidth(), bgImage.getHeight(), null);
-    for (StartingButton button : buttons) {
-    	button.paintComponent(g);
-    }
+		keyboard.setDimensions(250, 0, getWidth(), getHeight());
+		keyboard.paintComponent(g);
 	}
 
 	public void mouseClicked(MouseEvent e) {
-	    for (StartingButton button : buttons) {
-	    	button.setMouseClicked(e.getX(), e.getY());
-	    }
+		keyboard.mouseClicked(e);
 	}
 
-	public void setBGImage(BufferedImage image) {
-		if (image == null) {
-			bgImage = main_image;
-			return;
-		}
-		
-		bgImage = image;
-	}
-	
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		for (StartingButton button : buttons) {
-			button.computeMouseEntered(e.getX(), e.getY());
-			button.computeMouseExited(e.getX(), e.getY());
-		}
+		keyboard.mouseMoved(e);
 		this.repaint();
 	}
+
+	@Override
+	public void informKeyPressed(char keyPressed) {}
+
+	@Override
+	public void clearKeys() {}
 }
