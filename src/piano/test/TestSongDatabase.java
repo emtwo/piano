e5853d49fef863ae704714e71245886801712d11
Marina Samuel <marina.samuel25@gmail.com>
2013-03-20 15:46:12 -0400

    Fix existing unit tests.

--- src/piano/test/TestSongDatabase.java
+++ src/piano/test/TestSongDatabase.java
@@ -12,37 +12,40 @@ import piano.repository.Song;
 import piano.repository.SongDatabase;
 
 public class TestSongDatabase {
-/*
+
 	@Test
 	public void testStoreAndFetch() throws SQLException {
 		SongDatabase db = new SongDatabase();
 		db.wipeDatabase();
 
 		// Create strings to be inserted into the song database.
-		final String midiPath  = "music/midi/Sometimes.mid";
-		final String imagePath = "music/images/britney_spears.jpg";
-		final String title     = Song.getSongTitle(midiPath);
-		final String author    = "Britney Spears";
-		final String category  = "Pop";
+		final String title     = "Fur Elise";
+		final String subtitle  = "Subtitle";
+		final String composer  = "Beethoven";
+		final String style     = "Classical";
+		final String date      = "Some Date";
+		final String copyright = "1990";
+		final String name      = "id";
 
 		// Store and fetch from the song database.
-		db.store(title, "Britney Spears", midiPath, imagePath, category);
+		db.store(title, subtitle, composer, style, date, copyright, name);
 		ResultSet rs = db.fetchAllSongs();
 
 		// Test at least 1 row exists.
 		assertTrue(rs.next());
 
 		// Test that the row is the one we added.
-		assertEquals(midiPath,  rs.getString(SongDatabase.COL_MIDI_PATH));
-		assertEquals(imagePath, rs.getString(SongDatabase.COL_IMAGE_PATH));
-		assertEquals(title,     rs.getString(SongDatabase.COL_TITLE));
-		assertEquals(author,    rs.getString(SongDatabase.COL_AUTHOR));
-		assertEquals(category,  rs.getString(SongDatabase.COL_CATEGORY));
+		assertEquals(title, rs.getString(SongDatabase.COL_TITLE));
+		assertEquals(subtitle, rs.getString(SongDatabase.COL_SUBTITLE));
+		assertEquals(composer, rs.getString(SongDatabase.COL_COMPOSER));
+		assertEquals(style, rs.getString(SongDatabase.COL_STYLE));
+		assertEquals(date, rs.getString(SongDatabase.COL_DATE));
+		assertEquals(copyright, rs.getString(SongDatabase.COL_COPYRIGHT));
+		assertEquals(name, rs.getString(SongDatabase.COL_NAME));
 
 		// Test there are no more rows.
 		assertTrue(!rs.next());
 
 		db.closeConnection();
 	}
-	*/
 }
